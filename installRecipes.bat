6160da65658cf12877fbe035eff62557cd4e4bc0
Angelo Heringa <angeloheringa12@gmail.com>
2025-02-27 16:31:50 +0100

    update versions for install script

--- installRecipes.bat
+++ installRecipes.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set profiles=default 7_8_0 7_9_0 8_0_1 8_1_0 8_2_0 8_3_0 8_1_0 8_4_0 8_5_0 8_6_0 8_7_0 8_8_0 8_9_0 9_0_0
+set profiles=default 7_8 7_9_0 8_0_1 8_1_0 8_2_0 8_3_0 9_0_0
 
 for %%p in (%profiles%) do (
     if "%%p"=="default" (
